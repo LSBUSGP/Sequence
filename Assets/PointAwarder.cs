2031dd11b5a81d309b50a585b91a85ecada196b5
Paul Sinnett <paul.sinnett@gmail.com>
2020-03-06 16:26:22 +0000

    refactor

--- Assets/PointAwarder.cs
+++ Assets/PointAwarder.cs
@@ -4,366 +4,54 @@ using UnityEngine;
 
 public class PointAwarder : MonoBehaviour
 {
-    public bool Sequence1;
-    public bool Sequence2;
-    public bool Sequence3;
-    public bool Sequence4;
-    public bool Sequence5;
-    public bool Sequence6;
-
-    public bool Sequence12_press1;
-
-    public bool Sequence1_press2;
-    public bool Sequence1_press3;
-
-    public bool Sequence2_press2;
-    public bool Sequence2_press3;
-
-    public bool Sequence34_press1;
-
-    public bool Sequence3_press2;
-    public bool Sequence3_press3;
-
-    public bool Sequence4_press1;
-    public bool Sequence4_press2;
-    public bool Sequence4_press3;
-
-    public bool Sequence56_press1;
-
-    public bool Sequence5_press2;
-    public bool Sequence5_press3;
-
-    public bool Sequence6_press2;
-    public bool Sequence6_press3;
-
-    public float startTime = 0f;
-    public float timer = 0f;
-
-    public float holdTimePress = 0.1f;
-    public float holdTime1 = 1.0f;
-    public float holdTime2 = 2.0f;
-
-    // ***1***               if ((timer < (startTime + holdTimePress) && Input.GetKeyUp(KeyCode.Space)) && (Sequence34_press1 == false) && (Sequence56_press1 == false))
-    // ***2***               if ((timer > (startTime + holdTime1) && (timer < (startTime + holdTime2) && Input.GetKeyUp(KeyCode.Space)) && (Sequence12_press1 == false) && (Sequence56_press1 == false)))
-    // ***3***               if ((timer < (startTime + holdTime2) && Input.GetKeyUp(KeyCode.Space)) && (Sequence12_press1 == false) && (Sequence34_press1 == false))
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    //Update is called once per frame
-    void threefour()
-    {
-        Sequence34_press1 = true;
-    }
+    public string button = "Jump";
+    public float[] pressDurations = new float[] { 0.5f, 1.0f, 2.0f };
+    public List<string> sequences = new List<string>(
+        new string[] { "123", "132", "213", "231", "312", "321" }
+    );
+    float heldTime = 0.0f;
+    string sequence = "";
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetButton(button))
         {
-            startTime = Time.time;
-            timer = startTime;
+            heldTime += Time.deltaTime;
         }
-
-        if (Input.GetKey(KeyCode.Space))
+        else if (Input.GetButtonUp(button))
         {
-            timer += Time.deltaTime;
-        }
+            // identify duration
+            if (heldTime < pressDurations[0])
+            {
+                sequence += "1";
+            }
+            else if (heldTime < pressDurations[1])
+            {
+                sequence += "2";
+            }
+            else if (heldTime < pressDurations[2])
+            {
+                sequence += "3";
+            }
+            else
+            {
+                sequence += "X";
+            }
+            heldTime = 0.0f;
 
-        // ****************************************************************   1
-
-        if ((timer < (startTime + holdTimePress) && Input.GetKeyUp(KeyCode.Space)) && (Sequence34_press1 == false) && (Sequence56_press1 == false))
-        {
-            timer = 0f;
-            startTime = 0f;
-
-            Sequence12_press1 = true;
-
-            Debug.Log("Sequence12 Press1");
-        }
-
-
-        // 3-2
-
-        if ((Sequence12_press1 == true) && (timer > (startTime + holdTime2) && Input.GetKeyUp(KeyCode.Space)) && (Sequence2_press2 == false))
-        {
-            timer = 0f;
-            startTime = 0f;
-
-            Sequence1_press2 = true;
-
-            Debug.Log("Sequence1 Press2");
-        }
-        if ((Sequence1_press2 == true) && ((timer > (startTime + holdTime1) && (timer < (startTime + holdTime2) && Input.GetKeyUp(KeyCode.Space)) && (Sequence2_press2 == false))))
-        {
-            timer = 0f;
-            startTime = 0f;
-
-            Sequence1_press3 = true;
-
-            Debug.Log("Sequence3 Press3");
-
-        }
-        if (Sequence12_press1 && Sequence1_press2 && Sequence1_press3 == true)
-        {
-            Sequence1 = true;
-        }
-
-        // 2-3
-
-        if ((Sequence12_press1 == true) && ((timer > (startTime + holdTime1) && (timer < (startTime + holdTime2) && Input.GetKeyUp(KeyCode.Space)) && (Sequence1_press2 == false))))
-        {
-            timer = 0f;
-            startTime = 0f;
-
-            Sequence2_press2 = true;
-            Debug.Log("Sequence2 Press2");
-        }
-
-        if ((Sequence2_press2 == true) && (timer > (startTime + holdTime2) && Input.GetKeyUp(KeyCode.Space)) && (Sequence1_press2 == false))
-        {
-            timer = 0f;
-            startTime = 0f;
-
-            Sequence2_press3 = true;
-            Debug.Log("Sequence2 Press3");
-        }
-
-        if (Sequence12_press1 && Sequence2_press2 && Sequence2_press3 == true)
-        {
-            Sequence2 = true;
-        }
-
-        //***********************************************************************   2
-
-        if ((timer > (startTime + holdTime1) && (timer < (startTime + holdTime2) && Input.GetKeyUp(KeyCode.Space)) && (Sequence12_press1 == false) && (Sequence56_press1 == false)))
-        {
-            timer = 0f;
-            startTime = 0f;
-
-            Invoke("threefour", 0.1f);
-
-            Debug.Log("Sequence34 Press1");
-        }
-
-        // 3-1
-
-        if ((Sequence34_press1 == true) && (timer > (startTime + holdTime2) && Input.GetKeyUp(KeyCode.Space)))
-        {
-
-            timer = 0f;
-            startTime = 0f;
-
-            Sequence3_press2 = true;
-
-            Debug.Log("Sequence3 Press2");
-        }
-        if ((Sequence34_press1 == true) && (timer < (startTime + holdTimePress) && Input.GetKeyUp(KeyCode.Space)) && (Sequence4_press2 == false))
-        {
-            timer = 0f;
-            startTime = 0f;
-
-            Sequence3_press3 = true;
-
-            Debug.Log("Sequence3 Press3");
-        }
-
-        if (Sequence34_press1 && Sequence3_press2 && Sequence3_press3 == true)
-        {
-            Sequence3 = true;
-        }
-
-        // 1 - 3
-
-        if ((Sequence34_press1 == true) && (timer > (startTime + holdTime1) && (timer < (startTime + holdTime2) && Input.GetKeyUp(KeyCode.Space) && (Sequence3_press2 == false))))
-        {
-            timer = 0f;
-            startTime = 0f;
-
-            Sequence4_press2 = true;
-
-            Debug.Log("Sequence4 Press2");
-        }
-        if ((timer < (startTime + holdTime2) && Input.GetKeyUp(KeyCode.Space)) && (Sequence4_press2 == true) && (Sequence3_press2 == false))
-        {
-            timer = 0f;
-            startTime = 0f;
-
-            Sequence4_press3 = true;
-
-            Debug.Log("Sequence4 Press3");
-        }
-
-        if (Sequence34_press1 && Sequence3_press2 && Sequence3_press3 == true)
-        {
-            Sequence3 = true;
+            // check sequence
+            if (sequence.Length == 3)
+            {
+                if (sequences.Contains(sequence))
+                {
+                    Debug.LogFormat("Detected sequence {0}", sequence);
+                }
+                else
+                {
+                    Debug.LogFormat("Unknown sequence {0}", sequence);
+                }
+                sequence = "";
+            }
         }
     }
 }
-//        //3-2-1
-//        if (timer > (startTime + holdTime2) && Input.GetKeyUp(KeyCode.Space))
-//        {
-//            timer = 0f;
-//            startTime = 0f;
-
-//            Sequence6_press1 = true;
-
-//            if (Sequence6_press1 == true)
-//            {
-
-//                if (timer > (startTime + holdTime1) && Input.GetKeyUp(KeyCode.Space))
-//                {
-//                    timer = 0f;
-//                    startTime = 0f;
-
-//                    Sequence6_press2 = true;
-
-//                    if (Sequence6_press2 == true)
-//                    {
-
-//                        if (timer > (startTime + holdTimePress) && Input.GetKeyUp(KeyCode.Space))
-//                        {
-//                            timer = 0f;
-//                            startTime = 0f;
-
-//                            Sequence6_press3 = true;
-
-//                            if (Sequence6_press1 && Sequence6_press2 && Sequence6_press3 == true)
-//                            {
-//                                Sequence6 = true;
-
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//        else
-//        {
-//            //3-1-2
-//            if (timer > (startTime + holdTime2) && Input.GetKeyUp(KeyCode.Space))
-//            {
-//                timer = 0f;
-//                startTime = 0f;
-
-//                Sequence5_press1 = true;
-
-//                if (Sequence5_press1 == true)
-//                {
-//                    if (timer > (startTime + holdTime1) && Input.GetKeyUp(KeyCode.Space))
-//                    {
-//                        timer = 0f;
-//                        startTime = 0f;
-
-//                        Sequence5_press2 = true;
-
-//                        if (Sequence5_press2 == true)
-//                        {
-//                            if (timer > (startTime + holdTimePress) && Input.GetKeyUp(KeyCode.Space))
-//                            {
-//                                timer = 0f;
-//                                startTime = 0f;
-
-//                                Sequence5_press3 = true;
-
-//                                if (Sequence5_press1 && Sequence5_press2 && Sequence5_press3 == true)
-//                                {
-//                                    Sequence5 = true;
-
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//            else
-//            {
-//                ;
-//            }
-
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//            else
-//            {
-//                ;
-//            }
-
-//            //2-1-3
-//            if (timer > (startTime + holdTime1) && Input.GetKeyUp(KeyCode.Space))
-//            {
-//                timer = 0f;
-//                startTime = 0f;
-
-//                Sequence3_press1 = true;
-
-//                if (Sequence3_press1 == true)
-//                {
-//                    if (timer > (startTime + holdTimePress) && Input.GetKeyUp(KeyCode.Space))
-//                    {
-//                        timer = 0f;
-//                        startTime = 0f;
-
-//                        Sequence3_press2 = true;
-
-//                        if (Sequence3_press2 == true)
-//                        {
-//                            if (timer > (startTime + holdTime2) && Input.GetKeyUp(KeyCode.Space))
-//                            {
-//                                timer = 0f;
-//                                startTime = 0f;
-
-//                                Sequence3_press3 = true;
-
-//                                if (Sequence3_press1 && Sequence3_press2 && Sequence3_press3 == true)
-//                                {
-//                                    Sequence3 = true;
-
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//            else
-//            {
-//                ;
-//            }
-
-//            if ((Sequence1 == true) || (Sequence2 == true) || (Sequence3 == true) || (Sequence4 == true) || (Sequence5 == true) || (Sequence6 == true))
-//            {
-
-//                Sequence12_press1 = false;
-
-//                Sequence1_press2 = false;
-//                Sequence1_press3 = false;
-
-//                Sequence2_press2 = false;
-//                Sequence2_press3 = false;
-
-//                Sequence3_press1 = false;
-//                Sequence3_press2 = false;
-//                Sequence3_press3 = false;
-
-//                Sequence4_press1 = false;
-//                Sequence4_press2 = false;
-//                Sequence4_press3 = false;
-
-//                Sequence5_press1 = false;
-//                Sequence5_press2 = false;
-//                Sequence5_press3 = false;
-
-//                Sequence6_press1 = false;
-//                Sequence6_press2 = false;
-//                Sequence6_press3 = false;
-
-//            }
-//        }
-//    }
-//}
--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 # Sequence
-A sequence detection problem for specialism
 
-Initial code drop, not hooked into a project.
+Detect a sequence of 3 presses distinguished by the duration of the press. The program outputs the result on the console.
+
+This script has the following properties:
+
+- [Press Durations](#Press-Durations)
+- [Sequences](#Sequences)
+- [Button](#Button)
+
+## Press Durations
+
+The property `Press Durations` represents the maximum length of each duration in increasing order:
+
+1. short
+2. medium
+3. long
+
+The duration is in seconds and any time up to the first duration is identified as short `1`; up to the next as medium `2`; and so on. If a duration is longer than the maximum long duration, it is marked as an error `X`. Note that in the Unity editor the indices are 0, 1, and 2 because Unity indexes it's arrays from 0.
+
+## Sequences
+
+The property `Sequences` represents the sequences to detect. Each sequence is a string of 3 numbers representing a valid sequence of 3 detected durations. For example, the sequence `123` represents a short press, followed by a medium press, followed by a long press.
+
+## Button
+
+The property `Button` is the button mapped in the `Input` settings used to trigger the detection. You can set this to any button input that you have mapped.
+
+## How it works
+
+The program counts up the `heldTime` while the button is held down. When the button is released, it compares the `heldTime` against the `pressDurations` and adds to the currently recorded sequence. Finally, if the sequence has 3 identifiers, it compares against its list of known sequences to see if it has a match.
